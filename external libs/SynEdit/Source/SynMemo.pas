0b5ecd6b615e1ce6624e913050c8b4bc3513efca
c_schmitz <c_schmitz@fd5ee9c2-5e57-4641-80d3-f889442c0ae5>
2003-06-08 10:35:49 +0000

    Latest Synedit CVS version and LDraw Highlighter

---
+++ external libs/SynEdit/Source/SynMemo.pas
@@ -0,0 +1,62 @@
+{-------------------------------------------------------------------------------
+The contents of this file are subject to the Mozilla Public License
+Version 1.1 (the "License"); you may not use this file except in compliance
+with the License. You may obtain a copy of the License at
+http://www.mozilla.org/MPL/
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+the specific language governing rights and limitations under the License.
+
+The Original Code is: SynMemo.pas, released 2000-04-07.
+The Original Code is based on mwCustomEdit.pas by Martin Waldenburg, part of
+the mwEdit component suite.
+Portions created by Martin Waldenburg are Copyright (C) 1998 Martin Waldenburg.
+All Rights Reserved.
+
+Contributors to the SynEdit and mwEdit projects are listed in the
+Contributors.txt file.
+
+Alternatively, the contents of this file may be used under the terms of the
+GNU General Public License Version 2 or later (the "GPL"), in which case
+the provisions of the GPL are applicable instead of those above.
+If you wish to allow use of your version of this file only under the terms
+of the GPL and not to allow others to use your version of this file
+under the MPL, indicate your decision by deleting the provisions above and
+replace them with the notice and other provisions required by the GPL.
+If you do not delete the provisions above, a recipient may use your version
+of this file under either the MPL or the GPL.
+
+$Id: SynMemo.pas,v 1.1 2003-06-08 10:35:49 c_schmitz Exp $
+
+You may retrieve the latest version of this file at the SynEdit home page,
+located at http://SynEdit.SourceForge.net
+
+Known Issues:
+  - a handler for the different EM_XXX messages has to be implemented to make
+    this more compatible with TMemo
+-------------------------------------------------------------------------------}
+
+{$IFNDEF QSYNMEMO}
+unit SynMemo;
+{$ENDIF}
+
+{$I SynEdit.inc}
+
+interface
+
+uses
+{$IFDEF SYN_CLX}
+  QSynEdit;
+{$ELSE}
+  SynEdit;
+{$ENDIF}
+
+type
+  { most methods for compatibility with TMemo are already in TSynEdit }
+  TSynMemo = class(TSynEdit)
+  end;
+  
+implementation
+
+end.
